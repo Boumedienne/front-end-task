e0812a9bedf9013ada609a6770b03d0d47ca9525
Boumedienne <cb_bouzidi@esi.dz>
2019-11-21 04:50:46 +0100

    Adding Automatic generic scroll .#1

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Axios from 'axios';
-import { Header,Divider, Icon, Image, Item, Button, Container, Pagination, Placeholder } from 'semantic-ui-react';
-import { PlaceholderVimeo,ItemVimeo } from './ComponentVimeo'
+import { Message, Header, Divider, Icon, Image, Item, Button, Container, Pagination, Placeholder } from 'semantic-ui-react';
+import { PlaceholderVimeo, ItemVimeo } from './ComponentVimeo'
 
 
 
+//Global Paramaters
 const PATH_BASE = 'https://api.vimeo.com/channels/bestofthemonth/videos';
 const PARAM_PAGE = 'page=';
 const Authorization = 'bearer 57930b58a1a128c6c01489efd1ab3163'
-
+// Convert Time minute to HH:MM
 const timeconvert = (number) => {
   let hours = Math.floor(number / 60);
   let minutes = number % 60;
-  let str=minutes.toString();
-  if(minutes<10) str='0'.concat(str);
+  let str = minutes.toString();
+  if (minutes < 10) str = '0'.concat(str);
   return hours + ":" + str;
 }
 
-
+/**
+ * Main Page
+ */
 function App() {
   return (
     <>
@@ -31,56 +34,87 @@ function App() {
 
 
 
+/**
+ * Modal Container for listing Videos Information 
+ */
 class ModalContainer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       result: null,//Result for List videos 
+      resulScroll: [],
       error: null,//For Error Handling
       isLoadingContent: false,//For spinner
-      activePage: 1,
-      totalPages: null
+      activePage: 1,//Current Page
+      totalPages: null,//
+      onScroll: false,
+      usePagination: true,//Switch between Pagination and automatic scroll Listing 
+      isLoadingContentScroll: false,
+
     }
     //function to get page of videos 
     this.fetchBestVideos = this.fetchBestVideos.bind(this);
     this.setBestVideos = this.setBestVideos.bind(this);
+
   }
 
   setBestVideos(result) {
     this.setState({ result });
   }
 
-  fetchBestVideos(page = 2) {
-    let test = `${PATH_BASE}?${PARAM_PAGE}${page}`;
-    console.log(test);
+  fetchBestVideos(page = 1) {
+
     Axios.get(`${PATH_BASE}?${PARAM_PAGE}${page}`, { headers: { Authorization: Authorization } })
       .then(result => {
         //this.setBestVideos(result.data);
 
         this.setState({
           isLoadingContent: true,
+          activePage: page,
           result: result.data.data,
-          totalPages: result.data.per_page
+          totalPages: result.data.per_page,
+          resulScroll: [...this.state.resulScroll, ...result.data.data],
         });
-        console.log(result.data);
+
+      console.log(result);
 
       })
       .catch(error => this.setState({ error }));
   }
-//After mounting root node 
+  //After mounting root node 
   componentDidMount() {
     this.fetchBestVideos();
+    window.addEventListener('scroll', this.handleOnScroll);
+
+  }
+  //To handle Automatic Scroll 
+  handleOnScroll = () => {
+    let scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+    let scrollHeight = (document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight;
+    let clientHeight = document.documentElement.clientHeight || window.innerHeight;
+    let scrolledToBottom = Math.ceil(scrollTop + clientHeight) >= scrollHeight;
+
+    if (scrolledToBottom) {
+      this.fetchBestVideos(this.state.activePage + 1);
+      this.setState({
+        isLoadingContentScroll: true,
+        activePage: this.state.activePage + 1,
+        usePagination: false
+      });
+
+    }
 
   }
   //Pagination fire handeled 
   handlePaginationChange = (e, { activePage }) => {
     this.setState({
       isLoadingContent: false,
-       activePage:activePage
+      activePage: activePage,
+      usePagination: true
     });
     this.fetchBestVideos(activePage)
-    console.log(activePage);
-     
+    //console.log(activePage);
+
   }
   render() {
     const {
@@ -88,7 +122,10 @@ class ModalContainer extends React.Component {
       error,
       isLoadingContent,
       activePage,
-      totalPages
+      totalPages,
+      resulScroll,
+      usePagination,
+      isLoadingContentScroll
     } = this.state;
 
 
@@ -96,58 +133,49 @@ class ModalContainer extends React.Component {
       <div className='globalContainer'>
         <Header as='h1'> the best videos of the month in Vimeo </Header>
 
-        {error?<div>Somthing is wrong</div>
-     :isLoadingContent ? (
-      <Item.Group >
- 
-        {result.map(item =>
-    
-          <>
-            <ItemVimeo
-              imageurl={'https://i.vimeocdn.com'.concat(item.uri.split())}
-              name={item.name}
-              description={item.description}
-              duration={timeconvert(item.duration)}
-              language={item.language?item.language:'No Language '}
-              username={item.user.name}
-            />
-            <Divider />
+        {error ? <div>Somthing is wrong</div>
+          : isLoadingContent ? (
+            <Item.Group >
 
+              <PaginationOrScroll result={usePagination ? result : resulScroll} />
+              {isLoadingContentScroll ?
+                <Message>
 
-          </>
-        )}
-        
-      </Item.Group>
+                  <p>
+                    Is loading more videos
+    </p>
+                </Message> : null}
+            </Item.Group>
 
 
 
-    ) : (
-        <>
-          <PlaceholderVimeo />
-          <Divider style={{ margin:18}}/>
-          <PlaceholderVimeo />
-          <Divider style={{ margin:18}}/>
-          <PlaceholderVimeo />
-          <Divider style={{ margin:18}}/>
-          <PlaceholderVimeo />
-          <Divider style={{ margin:18}}/>
-          <PlaceholderVimeo />
+          ) : (
+              <>
+                <PlaceholderVimeo />
+                <Divider style={{ marginBottom: 24, Background: 'none' }} />
+                <PlaceholderVimeo />
+                <Divider style={{ marginBottom: 24, Background: 'none' }} />
+                <PlaceholderVimeo />
+                <Divider style={{ marginBottom: 24, Background: 'none' }} />
+                <PlaceholderVimeo />
+                <Divider style={{ marginBottom: 24, Background: 'none' }} />
+                <PlaceholderVimeo />
 
 
 
-        </>
+              </>
 
-      )
-    }
-          {totalPages&&<Pagination
-              //activePage={activePage}
-              activePage={activePage}
-              onPageChange={this.handlePaginationChange}
-              //defaultActivePage={5}
-              totalPages={totalPages}
-              ellipsisItem={false}
-              className={'pagination'}
-            />} 
+            )
+        }
+        {totalPages && <Pagination
+          //activePage={activePage}
+          activePage={activePage}
+          onPageChange={this.handlePaginationChange}
+          //defaultActivePage={5}
+          totalPages={totalPages}
+          ellipsisItem={false}
+          className={'pagination'}
+        />}
       </div>
 
     )
@@ -159,6 +187,27 @@ class ModalContainer extends React.Component {
 
 
 
+
+const PaginationOrScroll = (props) =>
+  <>
+    {props.result.map((item, i = 0) =>
+
+      <>
+        <ItemVimeo key={i++}
+          imageurl={'https://i.vimeocdn.com/video'.concat(item.pictures.uri.match(/(?<=pictures).\d*/g))}
+        
+          name={item.name}
+          description={item.description ? item.description : 'No description avaible'}
+          duration={timeconvert(item.duration)}
+          language={item.language ? item.language : 'No Language '}
+          username={item.user.name}
+        />
+        <Divider style={{ marginBottom: 24, Background: 'none' }} />
+
+
+      </>
+    )}
+  </>
 
 
 
--- src/ComponentVimeo.js
+++ src/ComponentVimeo.js
@@ -1,98 +1,105 @@
-import React  from 'react';
-import {  Popup,Button, Icon, Item, Label,Container,Header, Grid,Image ,Placeholder } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Loader, Popup, Button, Icon, Item, Label, Container, Header, Grid, Image, Placeholder } from 'semantic-ui-react';
 
 
 const getLimitText = (text, CharacterLimit) => {
-     let more=false;
-    if(text!=null){
-     if(text.length <CharacterLimit)  
+  let more = false;
+  if (text != null) {
+    if (text.length < CharacterLimit)
       return {
-       text:text,
-       more:more,
+        text: text,
+        more: more,
       };
-     else{
-        if ((text.constructor === String) ) {
-          more=true;
-          let str=text.slice(0, CharacterLimit).concat('...');
-          return {
-            text:str,
-            more:more,
-           };
-        }}}
-        else return  {
-          text:'',
-          more:more,
-         };
-    };
-
-export  const  PlaceholderVimeo  = ()=>(
-    <Grid>
-    <div>
-    <Placeholder>
-      <Placeholder.Image   style={{ height: 156, width: 280,top:10 }}/>
-    </Placeholder>
-   </div>
-    <Grid.Column width={12} style={{ marginTop:29}}> 
-    <Placeholder fluid>
-    <Placeholder.Paragraph>
-        <Placeholder.Line />
-        <Placeholder.Line />
-        <Placeholder.Line />
-        <Placeholder.Line />
-        <Placeholder.Line />
-      </Placeholder.Paragraph>
-    </Placeholder>
-    </Grid.Column>
-    
-  </Grid>
-  )
-
-  export  const  ItemVimeo  = (props)=>{
-    const {text, more}=getLimitText(props.description,400)
-   return( <Grid>
-    <div>
-      <Image src={props.imageurl} style={{ height: 156, width: 280 ,top:10}}/>
-      
-      <span className='duration'>{props.duration}</span>
-    </div>
-    <Grid.Column width={12} > 
-  <Header>{props.name}</Header>
-
-      <Container>
-       <p>
-      {
-       
-       more?(<>
-        {text} 
-        <Popup 
-        content={props.description}
-        on='click'
-        pinned
-        trigger={<a>   continue</a>} />
-        </>):text
-       
-     
+    else {
+      if ((text.constructor === String)) {
+        more = true;
+        let str = text.slice(0, CharacterLimit).concat('...');
+        return {
+          text: str,
+          more: more,
+        };
       }
+    }
+  }
+  else return {
+    text: '',
+    more: more,
+  };
+};
 
-       </p>
-    </Container>
-    <Item>
-    <Item.Extra>
-          <Label primary floated='right'>
-            <Icon name='language' />:
-            {props.language}
-            
-          </Label>
-          <Label >
-            <Icon name='user circle' />
-            {props.username}
-            
-          </Label>
-        </Item.Extra>
-
-    </Item>
+export const PlaceholderVimeo = () => (
+  <Grid className="carditem">
+    <div>
+      <Placeholder>
+        <Placeholder.Image style={{ height: 156, width: 280, top: 10 }} />
+      </Placeholder>
+    </div>
+    <Grid.Column width={12} style={{ marginTop: 29 }}>
+      <Placeholder fluid>
+        <Placeholder.Paragraph>
+          <Placeholder.Line />
+          <Placeholder.Line />
+          <Placeholder.Line />
+          <Placeholder.Line />
+          <Placeholder.Line />
+        </Placeholder.Paragraph>
+      </Placeholder>
     </Grid.Column>
-    
+
   </Grid>
-  )};
+)
+
+export const ItemVimeo = (props) => {
+  const { text, more } = getLimitText(props.description, 400)
+  const [isLoading, setisLoading] = useState(false);
+  const handleLoaded = () => {
+
+    setisLoading(true);
+
+  }
+  return (
+    <Grid className="carditem">
+
+      <div style={{ height: 156, width: 280, top: 10, display: isLoading ? "none" : "block" }}
+      >
+        <Loader active inline='centered' className="loaderimage" />
+      </div>
+      <div style={{ display: isLoading ? "block" : "none" }}
+      >
+        <Image
+          src={props.imageurl}
+          style={{ height: 156, width: 280, top: 10, display: isLoading ? "block" : "none" }}
+          onLoad={handleLoaded}
+        />
+
+        <span className='duration'>{props.duration}</span>*/}
+    </div>
+      <Grid.Column width={12} >
+        <Header>{props.name}</Header>
+
+        <Container>
+          <p>
+            {text}
+          </p>
+        </Container>
+        <Item>
+          <Item.Extra style={{top: 33,position: 'relative'}}>
+            <Label primary floated='right'>
+              <Icon name='language' />:
+            {props.language}
+
+            </Label>
+            <Label >
+              <Icon name='user circle' />
+              {props.username}
+
+            </Label>
+          </Item.Extra>
+
+        </Item>
+      </Grid.Column>
+
+    </Grid>
+  )
+};
 
